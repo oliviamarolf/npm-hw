3ebbb8353fb350c3226705181b8deeb3fbc32163
oliviamarolf <olivia@devpipeline.com>
2022-11-15 16:42:01 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const moment = require("moment");
+
+let hour = moment().hour();
+console.log(hour);
+
+let date = moment().date();
+console.log(date);
+
+let year = moment().year();
+console.log(year);
